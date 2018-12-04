cbcab39ebeef18cafe6542b5d462e9289ddf918d
dalongrong <1141591465@qq.com>
2018-12-04 20:05:48 +0800

    add responsee demo

--- app.lua
+++ app.lua
@@ -1,8 +1,13 @@
 function request(ctx, params)
-    ctx.serve({
-        status_code=302,
-        header={
-            location="http://www.baidu.com/",
-        },
-    })
+    -- ctx.serve({
+    --     status_code=302,
+    --     header={
+    --         location="http://www.baidu.com/",
+    --     },
+    -- })
+    ctx.request.header["user_token"]="request_token"
+end
+
+function response(ctx, params)
+    ctx.response.header["lua_token"]="dalongdemo";
 end
